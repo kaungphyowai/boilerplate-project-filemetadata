3e11baacad573dcbbbdd96fc7c05aba33f4580b0
Kaung Phyo Wai <kaungphyowai.mm@gmail.com>
2022-02-09 15:46:19 +0630

    finish

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config()
+var app = express();
+const multer  = require('multer')
+const upload = multer()
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile') , (req, res) => {
+  let file = req.file;
+  res.json({
+    "name": file.originalname,
+    "type": file.mimetype,
+    "size": file.size
+  })
+})
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
